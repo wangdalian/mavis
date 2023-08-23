2f018804231ebeed41df76b3be79b7dd79af7165
RIRU <riruoda@gmail.com>
2023-08-23 14:28:35 +0900

    impl simple memory allocator

--- kernel/main.c
+++ kernel/main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdint.h>
 
 extern char __bss[], __bss_end[], __stack_top[];
 
@@ -109,6 +110,21 @@ void handle_trap(struct trap_fram *f) {
     PANIC("unexpected trap scause=%x, stval=%x, sepc=%x\n", scause, stval, user_pc);
 }
 
+// memory allocator
+extern char __free_ram[], __free_ram_end[];
+
+paddr_t alloc_pages(uint32_t n) {
+    static paddr_t next_paddr = (paddr_t)__free_ram;
+    paddr_t paddr = next_paddr;
+    next_paddr += n * PAGE_SIZE;
+
+    if(next_paddr > (paddr_t) __free_ram_end)
+        PANIC("out of memory");
+
+    memset((void *)paddr, 0, n * PAGE_SIZE);
+    return paddr;    
+}
+
 void putchar(char ch) {
     sbi_call(ch, 0, 0, 0, 0, 0, 0, 1);
 }
@@ -117,7 +133,13 @@ void kernel_main(void) {
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
     
     WRITE_CSR(stvec, (uint32_t) kernel_entry);
-    __asm__ __volatile__("unimp"); // 無効な命令
+    
+    paddr_t paddr0 = alloc_pages(2);
+    paddr_t paddr1 = alloc_pages(1);
+    printf("alloc_pages test: paddr0=%x\n", paddr0);
+    printf("alloc_pages test: paddr1=%x\n", paddr1);
+
+    PANIC("booted!");
 }
 
 __attribute__((section(".text.boot")))
--- libs/common.h
+++ libs/common.h
@@ -4,6 +4,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define PAGE_SIZE   4096
+
 // clang extention
 #define align_up(value, align) __builtin_align_up(value, align)
 #define is_aligned(value, align) __builtin_is_aligned(value, align)
