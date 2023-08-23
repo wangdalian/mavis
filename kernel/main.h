3e730d63558f01e5164afaa42500a3a4aef8592a
RIRU <riruoda@gmail.com>
2023-08-23 14:00:09 +0900

    impl kernel panic

--- kernel/main.h
+++ kernel/main.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#define PANIC(fmt, ...)                                                         \
+    do {                                                                        \
+        printf("PANIC %s:%d " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);     \
+        for(;;)                                                                 \
+            __asm__ __volatile__("wfi");                                        \
+    }while(0)
+
 struct sbiret {
     long error;
     long value;
