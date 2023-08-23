544919d9f1d19ac94372c2d266a96a121d69ce8b
RIRU <riruoda@gmail.com>
2023-08-23 13:46:08 +0900

    impl some util functions

--- libs/common.c
+++ libs/common.c
@@ -1,6 +1,41 @@
 #include "common.h"
+#include <stdint.h>
 
 void putchar(char ch);
+
+void *memset(void *buf, char c, size_t n) {
+    uint8_t *p = buf;
+    while(n--)
+        *p++ = c;
+    return buf;
+}
+
+void *memcpy(void *dst, const void *src, size_t n) {
+    uint8_t *d = dst;
+    const uint8_t *s = src;
+    while(n--)
+        *d++ = *s++;
+    return dst;
+}
+
+char *strcpy(char *dst, const char *src) {
+    char *d = dst;
+    while(*src)
+        *d++ = *src++;
+    *d = '\0';
+    return dst;
+}
+
+int strcmp(const char *s1, const char *s2) {
+    while(*s1 && *s2) {
+        if(*s1 != *s2)
+            break;
+        s1++;
+        s2++;
+    }
+
+    return *s1 - *s2;
+}
 
 void printf(const char *fmt, ...) {
     va_list vargs;
--- libs/common.h
+++ libs/common.h
@@ -1,8 +1,18 @@
 #pragma once
 
-#define va_list  __builtin_va_list
-#define va_start __builtin_va_start
-#define va_end   __builtin_va_end
-#define va_arg   __builtin_va_arg
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
 
+// clang extention
+#define align_up(value, align) __builtin_align_up(value, align)
+#define is_aligned(value, align) __builtin_is_aligned(value, align)
+
+typedef uintptr_t paddr_t;
+typedef uintptr_t vaddr_t;
+
+void *memset(void *buf, char c, size_t n);
+void *memcpy(void *dst, const void *src, size_t n);
+char *strcpy(char *dst, const char *src);
+int strcmp(const char *s1, const char *s2);
 void printf(const char *fmt, ...);
