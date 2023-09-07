e5476f671eeba0ea6898af5956248a83336fdfe6
RIRU <riruoda@gmail.com>
2023-09-07 16:52:50 +0900

    impl memset

---
+++ lib/memset.c
@@ -0,0 +1,8 @@
+#include "string.h"
+
+void *memset(void *buf, char c, size_t n) {
+    uint8_t *p = (uint8_t *) buf;
+    while (n--)
+        *p++ = c;
+    return buf;
+}
--- lib/string.h
+++ lib/string.h
@@ -1,3 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+#include <stddef.h>
+
+void *memset(void *buf, char c, size_t n);
 int strcmp(const char *s1, const char *s2);
